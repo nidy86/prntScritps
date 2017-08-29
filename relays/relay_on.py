4cfab7d4c3391979d1d195be9d1a083a19349f37
patrik <patrik@patriknchersmbp>
2017-08-29 10:37:31 +0200

    BCM 14

--- relays/relay_on.py
+++ relays/relay_on.py
@@ -3,7 +3,6 @@ GPIO.setmode(GPIO.BCM) # GPIO Nummern statt Board Nummern
  
 RELAIS_1_GPIO = 14 
 GPIO.setup(RELAIS_1_GPIO, GPIO.OUT) # GPIO Modus zuweisen
-GPIO.output(RELAIS_1_GPIO, GPIO.HIGH) # aus
 GPIO.output(RELAIS_1_GPIO, GPIO.LOW) # an
 
 GPIO.cleanup()
