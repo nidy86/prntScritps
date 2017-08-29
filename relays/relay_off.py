285ed0c915e3f0f32fff2a6473c5c5c3c9db2577
patrik <patrik@patriknchersmbp>
2017-08-29 10:39:49 +0200

    Without warnings

--- relays/relay_off.py
+++ relays/relay_off.py
@@ -1,7 +1,8 @@
 import RPi.GPIO as GPIO
+GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM) # GPIO Nummern statt Board Nummern
  
-RELAIS_1_GPIO = 14
+RELAIS_1_GPIO = 15
 GPIO.setup(RELAIS_1_GPIO, GPIO.OUT) # GPIO Modus zuweisen
 GPIO.output(RELAIS_1_GPIO, GPIO.LOW) # an
 GPIO.output(RELAIS_1_GPIO, GPIO.HIGH) # aus
--- relays/relay_on.py
+++ relays/relay_on.py
@@ -1,8 +1,9 @@
 import RPi.GPIO as GPIO
+GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM) # GPIO Nummern statt Board Nummern
  
-RELAIS_1_GPIO = 14 
+RELAIS_1_GPIO = 15
 GPIO.setup(RELAIS_1_GPIO, GPIO.OUT) # GPIO Modus zuweisen
 GPIO.output(RELAIS_1_GPIO, GPIO.LOW) # an
 
-GPIO.cleanup()
+#GPIO.cleanup()
