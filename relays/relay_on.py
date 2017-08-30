ac66fb3648391b2419b0983d8c5c8f9bffbc1c1d
patrik <patrik@patriknchersmbp>
2017-08-30 17:22:59 +0200

    bash scripts

--- relays/relay_on.py
+++
@@ -1,9 +0,0 @@
-import RPi.GPIO as GPIO
-GPIO.setwarnings(False)
-GPIO.setmode(GPIO.BCM) # GPIO Nummern statt Board Nummern
- 
-RELAIS_1_GPIO = 18
-GPIO.setup(RELAIS_1_GPIO, GPIO.OUT) # GPIO Modus zuweisen
-GPIO.output(RELAIS_1_GPIO, GPIO.LOW) # an
-
-#GPIO.cleanup()
---
+++ scripts/startup.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+python /home/pi/scripts/prntScritps/lights/startup.py
+python /home/pi/scripts/prntScritps/relays/switch_reset.py
