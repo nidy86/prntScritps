fec18c4c96acc7f7a1331f70c5169a0593c6a678
patrik <patrik@patriknchersmbp>
2017-08-30 16:44:10 +0200

    ...

---
+++ relays/relay_reset.py
@@ -0,0 +1,24 @@
+import sys, getopt
+import RPi.GPIO as GPIO
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM) # GPIO Nummern statt Board Nummern
+
+RELAY = [14,15,17,18]
+
+def off(relID):
+    RELAY_GPIO_ID = relID
+    GPIO.setup(RELAY_GPIO_ID, GPIO.OUT) # GPIO Modus zuweisen
+    GPIO.output(RELAY_GPIO_ID, GPIO.LOW) # an
+    GPIO.output(RELAY_GPIO_ID, GPIO.HIGH) # aus
+    return
+
+
+
+if __name__ == "__main__":
+    try:
+        for i in range(len(RELAY)):
+            off(relay[i])
+        GPIO.cleanup()
+    except KeyboardInterrupt:
+        print("User stop")
+        GPIO.cleanup()
